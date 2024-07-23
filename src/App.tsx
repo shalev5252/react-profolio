facb96bbff10e6074774e3fd246c2b01c551f421
shalev5252 <shalev@campus.technion.ac.il>
2024-07-23 20:25:53 +0300

    added readme file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
-// import About from "./components/About/About";
+import About from "./components/About/About";
 import Experience from "./components/Experience/Experience";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
